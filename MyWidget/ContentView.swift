bde8313fae2b86b6a99848177dce88e3ff7a2dc4
Eяw!и Gontier <ierwinx@me.com>
2022-09-02 16:58:32 -0500

    Initial Commit

---
+++ MyWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWidget
+//
+//  Created by Erwin Luz León on 02/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWidget/MyWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWidgetApp.swift
+//  MyWidget
+//
+//  Created by Erwin Luz León on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
